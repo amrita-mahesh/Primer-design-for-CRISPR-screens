3331bd388f8b27dc83388ab162651981540be340
Amrita Mahesh <74362099+amrita-mahesh@users.noreply.github.com>
2023-05-24 17:30:15 +0530

    Added files for primer design

---
+++ NLR_flanking_sequences_check.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 21 15:25:51 2022
+
+Check for sequences of a given range of lengths that are common on either side of a gene.
+
+Input:
+    1. 
+
+@author: Amrita
+"""
